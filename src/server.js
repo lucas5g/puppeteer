4ac799c8a162889268d295a0cc7ac5a71ba014a7
Lucas <lucasdesousa19@hotmail.com>
2020-07-30 14:24:31 -0300

    make map thuerday

--- src/server.js
+++ src/server.js
@@ -5,70 +5,34 @@ import Robo from './robo'
 
 const menu = async () => {
 
-
-
-
     const coursesDay = diaryDay[nameDay()]
+    // console.log(nameDay())
+    console.log({coursesDay})
     const diaryDayUrl = coursesDay.map((courseDay, index) => {
-        // console.log({courseDay}, {index})
-        // console.log(Object.values(courses[index]))
         const teste = courses.find(course => {
             if (course.name === courseDay) {
                 return course
             }
         })
-
         return teste
+    })
+
+    let time = 0
+    console.log({ diaryDayUrl })
+    diaryDayUrl.forEach(course => {
+
+        setTimeout(() => {
+
+            let name = course.name
+            let url = course.url
+            let robo = new Robo(name, url)
+            // robo.setup()
+            console.log({ name })
+        }, time)
+        time+=35000
 
     })
-    // console.log({ coursesDay })
-    // console.log({ diaryDayUrl })
-    // let time = 14000
-        let time = 0
-        diaryDayUrl.forEach(course => {
 
-            setTimeout(() => {
-
-                let name = course.name
-                let url = course.url
-                let robo = new Robo(name, url)
-                console.log({ name })
-            }, time)
-            time+=15000
-
-        })
-
-    // let name = diaryDayUrl[0].name
-    // let url = diaryDayUrl[0].url
-    // let robo = new Robo(name, url)
-    // robo.setup()
-
-    // console.log({name, url})
-
-
-
-    // const diaryDayUrl = courses.map(course => {
-
-    //   console.log(course.name)
-    //   // console.log(course.name, diaryDay[nameDay()][course.name])
-    //   let courseVerifyDay = diaryDay[nameDay()].find(item => {
-    //     // console.log('item ', item)
-    //     // console.log('course', course.name)
-    //     // console.log('true ', item === course.name)
-    //     return item === course.name
-    //   })
-
-    //   if (course.name === courseVerifyDay ) {
-    //     return course
-    //   }
-    //   return course === diaryDay[course]
-    // })
-
-    // console.log(diaryDayUrl)
-
-
-
-    // robo.setup()
 
 
 }
--- src/service/data.ts
+++ src/service/data.ts
@@ -4,24 +4,43 @@ dotenv.config()
 
 export const courses = [
     { name: 'teste', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=81&section=5&singlesec=5' },
-    { name: 'matematicaVM1N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=50&section=1&notifyeditingon=1' },
-    { name: 'matematicaVM2N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=51&section=1&singlesec=1' },
+
+    { name: 'biologiaVM1N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=71&section=1&singlesec=1'},
+    { name: 'biologiaVM2N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=72&section=1&singlesec=1' },
+    { name: 'biologiaVM3N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=73&section=1&notifyeditingon=1' },
 
     { name: 'espanholVM1N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=62&section=1' },
     { name: 'espanholVM2N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=63&section=1&singlesec=1' },
     { name: 'espanholVM3N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=64&section=1&singlesec=1' },
+    
+    { name: 'filosofiaVM1N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=78&section=1'},
+    { name: 'filosofiaVM2N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=79&section=1&singlesec=1' },
+    { name: 'filosofiaVM3N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=80&section=1&singlesec=1'},
+    
+    { name: 'fisicaVM1N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=65&section=1&singlesec=1'},
+
+
+    { name: 'geografiaVM2N' , url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=57&section=1'},
+    
+    { name: 'historioVM3N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=55&section=1&singlesec=1' },
+
+    { name: 'matematicaVM1N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=50&section=1&notifyeditingon=1' },
+    { name: 'matematicaVM2N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=51&section=1&singlesec=1' },
+    { name: 'matematicaVM3N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=52&section=1&singlesec=1'},
+
 
     { name: 'portuguesVM1N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=47&section=1&singlesec=1' },
     { name: 'portuguesVM2N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=48&section=1' },
     { name: 'portuguesVM3N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=49&section=1&notifyeditingon=1' },
 
-    { name: 'historioVM3N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=55&section=1&singlesec=1' },
 
+    { name: 'quimicaVM1N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=68&section=1&singlesec=1' },
+    { name: 'quimicaVM2N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=69&section=1'},
     { name: 'quimicaVM3N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=70&section=1&notifyeditingon=1' },
+    
+    { name: 'sociologiaVM2N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=76&section=1&singlesec=1'}, 
+    { name: 'sociologiaVM3N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=77&section=1&singlesec=1' },
 
-    { name: 'fisicaVM1N', url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=65&section=1&singlesec=1'},
-
-    { name: 'geografiaVM2N' , url: 'https://meuvicuna.freicarlosvicuna.com.br/course/view.php?id=57&section=1'}
 
 
 ]
@@ -29,9 +48,17 @@ export const courses = [
 
 export const diaryDay = {
     Tuesday : [
-        // 'matematicaVM2N',
+        'matematicaVM2N',
          'quimicaVM3N', 'fisicaVM1N',
         'portuguesVM3N', 'matematicaVM1N', 'geografiaVM2N'
+    ],
+    Wednesday: [
+        'matematicaVM1N', 'filosofiaVM2N',  'biologiaVM3N',
+        'quimicaVM1N', 'biologiaVM2N', 'filosofiaVM3N', 
+    ],
+    Thursday:[
+        'quimicaVM3N', 'filosofiaVM1N', 'matematicaVM2N',
+        'biologiaVM1N', 'matematicaVM3N', 'quimicaVM2N'
     ],
     teste : [
         'matematicaVM2N', 'quimicaVM3N', 'fisicaVM1N',
